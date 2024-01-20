50ce512eed5b9fb00f9bf36820f5c9d5badb6b69
VitalySavilov <vitalik.savilov@gmail.com>
2024-01-20 21:32:49 +0300

    added input data validation

--- service/src/main/java/it/academy/dto/AppUserCreateDto.java
+++ service/src/main/java/it/academy/dto/AppUserCreateDto.java
@@ -1,9 +1,29 @@
 package it.academy.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record AppUserCreateDto(
+        @NotBlank(message = "Lastname should be filled in")
+        @Size(max = 40, message = "Lastname must be no longer than 40 characters")
+        @Pattern(regexp = "^[a-zA-Z]+$", message = "Lastname must contain only latin letters")
         String lastname,
+        @NotBlank(message = "Firstname should be filled in")
+        @Size(max = 20, message = "Firstname must be no longer than 20 characters")
+        @Pattern(regexp = "^[a-zA-Z]+$", message = "Firstname must contain only latin letters")
         String firstname,
+        @NotBlank(message = "Patronymic should be filled in")
+        @Size(max = 40, message = "Patronymic must be no longer than 40 characters")
+        @Pattern(regexp = "^[a-zA-Z]+$", message = "Patronymic must contain only latin letters")
         String patronymic,
+        @Email(message = "Email must be in the format of an email address")
+        @NotBlank(message = "Email should be filled in")
+        @Size(max = 50, message = "Email must be no longer than 40 characters")
         String email,
+        @NotBlank(message = "Role should be filled in")
+        @Pattern(regexp = "Administrator|Sale User|Customer User|Secure API User",
+                message = "Role variants: Administrator, Sale User, Customer User, Secure API User")
         String role) {
 }
--- web/src/main/java/it/academy/rest/AppUserRestController.java
+++ web/src/main/java/it/academy/rest/AppUserRestController.java
@@ -6,6 +6,7 @@ import it.academy.service.AppUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +19,7 @@ public class AppUserRestController {
     private final AppUserService appUserService;
 
     @PostMapping
-    public ResponseEntity<AppUserReadDto> postUser(@RequestBody AppUserCreateDto appUserCreateDto) {
+    public ResponseEntity<AppUserReadDto> postUser(@RequestBody @Validated AppUserCreateDto appUserCreateDto) {
         return new ResponseEntity<>(
                 appUserService.createAppUser(appUserCreateDto),
                 HttpStatus.CREATED);
